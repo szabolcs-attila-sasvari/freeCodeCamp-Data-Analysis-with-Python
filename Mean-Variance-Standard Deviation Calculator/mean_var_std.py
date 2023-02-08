262ae62d57f9f988c58457029d09ed2a62d39acd
Szabolcs Attila Sasvári <113426911+szabolcs-attila-sasvari@users.noreply.github.com>
2023-02-08 10:01:03 +0100

    Update mean_var_std.py

--- Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,1 +1,20 @@
+import numpy as np
 
+
+def calculate(list):
+    if len(list) == 9:
+        one_d = np.array(list)
+        np_array = one_d.reshape(3, 3)
+
+        calculations = {
+            'mean': [np.mean(np_array, axis=0).tolist(), np.mean(np_array, axis=1).tolist(), np.mean(np_array.flatten())],
+            'variance': [np.var(np_array, axis=0).tolist(), np.var(np_array, axis=1).tolist(), np.var(np_array.flatten())],
+            'standard deviation': [np.std(np_array, axis=0).tolist(), np.std(np_array, axis=1).tolist(), np.std(np_array.flatten())],
+            'max': [np.max(np_array, axis=0).tolist(), np.max(np_array, axis=1).tolist(), np.max(np_array.flatten())],
+            'min': [np.min(np_array, axis=0).tolist(), np.min(np_array, axis=1).tolist(), np.min(np_array.flatten())],
+            'sum': [np.sum(np_array, axis=0).tolist(), np.sum(np_array, axis=1).tolist(), np.sum(np_array.flatten())]
+        }
+        return calculations
+
+    else:
+        raise ValueError("List must contain nine numbers.")
