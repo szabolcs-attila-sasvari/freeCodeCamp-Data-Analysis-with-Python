5ca88e8006d3fe1b48a53c84bba9557c15b96998
Szabolcs Attila Sasvári <113426911+szabolcs-attila-sasvari@users.noreply.github.com>
2023-02-22 21:05:47 +0100

    Update medical_data_visualizer.py

--- Medical Data Visualizer/medical_data_visualizer.py
+++ Medical Data Visualizer/medical_data_visualizer.py
@@ -1,1 +1,52 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Import data
+df = pd.read_csv("medical_examination.csv")
+
+# Add 'overweight' column
+BMI = df['weight'] / np.power(df['height'] / 100, 2)
+df['overweight'] = np.where(25 >= BMI, 0, 1)
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
+
+# Draw Categorical Plot
+def draw_cat_plot():
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+
+    # Get the figure for the output
+    catplot = sns.catplot(df_cat, x="variable", hue="value", col="cardio", kind="count")
+    catplot.set_ylabels("total")
+    fig = catplot.figure
+
+    # Do not modify the next two lines
+    fig.savefig('catplot.png')
+    return fig
+
+
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) &
+                 (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr))
+
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, annot=True, fmt=".1f", mask=mask)
+    fig = plt.gcf()
+    plt.show()
+
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
